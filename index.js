13063d745ff3330b0580f6fc1a8c99fb837fb7f2
Daniel Augusto <daniel.arquiteto@gmail.com>
2020-01-21 16:35:30 -0300

    Configurações iniciais

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const connection = require("./database/database");
+
+//View engine
+app.set("view engine", "ejs");
+
+//Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Static
+app.use(express.static("public"));
+
+//Database
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso");
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(8000, () => {
+  console.log("Servidor está rodando");
+});
